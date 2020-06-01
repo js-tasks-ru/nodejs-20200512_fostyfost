a6b0f87705baa34ed60616e8e8005533c7dd7049
Fosty Fost <fostyfost@gmail.com>
2020-06-01 10:28:30 +0300

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,19 @@
-module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
-};
+const CategoryModel = require('../models/Category')
+
+const normalizeCategory = category => {
+  return {
+    id: category._id,
+    title: category.title,
+    subcategories: category.subcategories.map(subcategory => ({
+      id: subcategory._id,
+    })),
+  }
+}
+
+module.exports.categoryList = async ctx => {
+  const categories = await CategoryModel.find()
+
+  ctx.body = {
+    categories: categories.map(normalizeCategory)
+  }
+}
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,61 @@
-module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
-};
+const mongoose = require('mongoose')
 
-module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
-};
+const ProductModel = require('../models/Product')
 
-module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+const normalizeProduct = product => {
+  return {
+    id: product._id,
+    title: product.title,
+    description: product.description,
+    price: product.price,
+    category: product.category,
+    subcategory: product.subcategory,
+    images: product.images,
+  }
+}
+
+const isValidObjectId = id => {
+  return mongoose.Types.ObjectId.isValid(id) && (new mongoose.Types.ObjectId(id)).toString() === id
+}
+
+module.exports.productsBySubcategory = async (ctx, next) => {
+  if (!ctx.query.subcategory) {
+    return next()
+  }
+
+  if (isValidObjectId(ctx.query.subcategory)) {
+    const products = await ProductModel.find({ subcategory: ctx.query.subcategory })
+
+    ctx.body = {
+      products: products.map(normalizeProduct)
+    }
+  } else {
+    ctx.status = 400
+  }
+}
+
+module.exports.productList = async ctx => {
+  const products = await ProductModel.find()
+
+  ctx.body = {
+    products: products.map(normalizeProduct)
+  }
+}
+
+module.exports.productById = async ctx => {
+  if (!isValidObjectId(ctx.params.id)) {
+    ctx.status = 400
+    return;
+  }
+
+  const product = await ProductModel.findById(ctx.params.id)
+
+  if (product) {
+    ctx.body = {
+      product: normalizeProduct(product),
+    }
+  } else {
+    ctx.status = 404
+  }
+}
 
